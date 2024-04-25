451626bb91ae15e6d126ca916603cd5601a83c0d
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-04-25 10:55:54 +0800

    Add test endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+  res.send('Test!')
+})
+
 app.listen(5000, () => {
   console.log('Example app listening on port 3000!')
 })
